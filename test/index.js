33a871eae9e1afe866f0f40c3f64904133a813fe
androidgrl <kawaharajamie@gmail.com>
2015-10-28 14:05:12 -0600

    Add square placePiece function

--- test/index.js
+++ test/index.js
@@ -1,14 +1,6 @@
-const chai = require('chai');
-const assert = chai.assert;
 require('./board_test');
 require('./square_test');
 require('./piece_test');
 require('./game_test');
 require('./player_test');
 require('./evaluate-flippable-path_test');
-
-describe('my test suite', function () {
-  xit('should work yo', function () {
-    assert(true);
-  });
-});
